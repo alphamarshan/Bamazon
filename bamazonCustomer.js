f518cbd6e738a5895ca84b728beb749787581ad9
Andrew Marsh <andrewmarsh42@outlook.com>
2017-12-05 22:08:15 -0500

    last changes

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,11 +1,12 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
 
+// You will need your own username/password to access the mySQL database
 var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
-  password: "Fini$hing21",
+  password: "",
   database: "bamazon"
 });
 
